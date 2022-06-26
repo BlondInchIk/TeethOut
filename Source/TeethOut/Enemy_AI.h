319b9629a913d2e52249bee7f253d8516ff0ea21
BlondInchIk <ampavlenko@miem.hse.ru>
2022-06-26 12:24:43 +0300

    Make CMake

---
+++ Source/TeethOut/Enemy_AI.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TEETHOUT_API Enemy_AI
+{
+public:
+	Enemy_AI();
+	~Enemy_AI();
+};
